ec80e283054791e0b642a8789f90ac20034da1a9
Luca Lambia <luca@lambia.it>
2026-02-13 10:53:45 +0100

    minor

--- query usate/5. vista.sql
+++ query usate/5. vista.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW luoghi_arricchiti AS
+CREATE OR REPLACE VIEW v_luoghi AS
 SELECT 
     id,
     nome,
@@ -8,7 +8,7 @@ SELECT
     ST_AsText(posizione) AS posizione_testo,
     
     -- Coordinate in lat/lon (SRID 4326)
-    ST_Transform(posizione, 4326) AS posizione_latlon,
+    ST_AsText(ST_Transform(posizione, 4326)) AS posizione_latlon,
     
     -- Distanza da Torino (coordinate centro approssimativo 1500000, 5100000 in 3003)
     ST_Distance(
@@ -23,6 +23,6 @@ SELECT nome, posizione_testo,
        ST_X(posizione_latlon) AS lon, 
        ST_Y(posizione_latlon) AS lat,
        distanza_da_torino_m
-FROM luoghi_arricchiti
+FROM v_luoghi
 ORDER BY distanza_da_torino_m
 LIMIT 10;
